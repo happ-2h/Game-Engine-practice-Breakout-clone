2b519a326a263830ca69e2075651ca59c4ad3c2a
happ-2h <hikonetgames@gmail.com>
2024-08-30 19:03:42 -0700

    KeyHandler class created

---
+++ src/input/KeyHandler.js
@@ -0,0 +1,39 @@
+let instance = null;
+
+class _KeyHandler {
+  #keys; // Holds pressed key status
+
+  constructor() {
+    if (instance) throw new Error("KeyHandler singleton reconstructed");
+
+    this.#keys = [];
+
+    onkeydown = this.#keyDown.bind(this);
+    onkeyup   = this.#keyDown.bind(this);
+
+    instance = this;
+  }
+
+  #keyDown(e) {
+    e.preventDefault();
+
+    /*
+     * true:  key is down
+     * false: key is up
+     */
+    this.#keys[e.keyCode] = e.type === "keydown";
+  }
+
+  /**
+   *
+   * @param {Number} keycode - Keycode value
+   *
+   * @returns true if key is down; false otherwise
+   */
+  isDown(keycode) {
+    return this.#keys[keycode];
+  }
+};
+
+const KeyHandler = new _KeyHandler;
+export default KeyHandler;
